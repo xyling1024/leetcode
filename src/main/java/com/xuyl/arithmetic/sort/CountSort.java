7162b882d0c5265bb0e1c9ffa37955c460159e5b
yaling.xu <yaling.xu@appshahe.com>
2019-11-20 21:48:29 +0800

    各种排序

---
+++ src/main/java/com/xuyl/arithmetic/sort/CountSort.java
@@ -0,0 +1,10 @@
+package com.xuyl.arithmetic.sort;
+
+/**
+ * @description: 计数排序
+ * @author: yaling.xu
+ * @signature 天王盖地虎，胖墩吃脑腐。
+ * @date: 2019/11/20 21:47
+ */
+public class CountSort {
+}
--- src/main/java/com/xuyl/arithmetic/sort/QuickSort.java
+++ src/main/java/com/xuyl/arithmetic/sort/QuickSort.java
@@ -12,6 +12,9 @@ import java.util.LinkedList;
  */
 public class QuickSort {
 
+//    private int[] nums = {453, 75, 223, 89, 23, 0};
+    private int[] nums = {423, 453, 75, 23, 89, 223, 75, 0, 5};
+
     /**
      * 快速排序：任选数组中一个对象，将比这个对象大的扔到对象的左边，小的放到对象的右边。
      *          对已经分好的对象左右两边的数组继续进行这个操作，直到最后两边都只剩一个对象，整个数组就变成有序的了
@@ -22,13 +25,12 @@ public class QuickSort {
      *      不断循环以上过程, 直到 i == j, 说明数组中所有对象都已比较完毕.
      *      此时, 原数组已经被我们分成了: 比临时变量小的数组 + 临时变量 + 比临时变量大的数组
      *      对除临时变量外的两个数组重复进行如上步骤, 直到数组的长度小于等于1, 长度为1的数组天然有序, 那么整体数组也就有序了.
-     * @param nums
      * @return
      */
-    public int[] quickSort(int[] nums, int start, int end) {
+    public void quickSort(int start, int end) {
         // 长度为1的数组天然有序, 直接返回
         if ( end - start < 1 ) {
-            return nums;
+            return;
         }
         int tmpStart = start, tmpEnd = end;
         // 第一个元素作为分界对象
@@ -38,17 +40,15 @@ public class QuickSort {
         while (true) {
             if (!compareStart) {
                 if (tmp > nums[end]) {
-                    nums[start] = nums[end];
-                    compareStart = true;
-                    start++;
+                    nums[start++] = nums[end];
+                    compareStart = !compareStart;
                 } else {
                     end--;
                 }
             } else {
                 if (tmp < nums[start]) {
-                    nums[end] = nums[start];
-                    compareStart = false;
-                    end--;
+                    nums[end--] = nums[start];
+                    compareStart = !compareStart;
                 } else {
                     start++;
                 }
@@ -58,21 +58,17 @@ public class QuickSort {
             }
         }
         nums[start] = tmp;
-        quickSort(nums, tmpStart, start - 1);
-        quickSort(nums, end + 1, tmpEnd);
-        return nums;
+        quickSort(tmpStart, start - 1);
+        quickSort(end + 1, tmpEnd);
     }
 
     @Test
     public void test() {
-        int[] nums = {453, 75, 223, 89, 23, 0};
-//        int[] nums = {423, 453, 75, 23, 89, 223, 75, 0, 5};
-        int[] ints = quickSort(nums, 0, nums.length - 1);
-        for (int num : ints) {
+        quickSort(0, nums.length - 1);
+        for (int num : nums) {
             System.out.println(num);
         }
     }
-
 
 
 
